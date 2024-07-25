116b593911e1a7b3df18f6656513220e46a62c70
Harlequelrah <degbovimax19@gmail.com>
2024-07-25 17:02:27 +0000

    executions de quelques migrations

---
+++ Migrations/20240725151937_update.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace giftcard_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateExpirations",
+                table: "Packages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateInscription",
+                table: "Users",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateSouscription",
+                table: "Subscriptions",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateExpiration",
+                table: "Subscriptions",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "NbrCarteGenere",
+                table: "Subscriptions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "SubscriberHistories",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxCarte",
+                table: "Packages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Packages",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NbrJour",
+                table: "Packages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomPackage",
+                table: "Packages",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "MerchantHistories",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "BeneficiaryHistories",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateExpiration",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "NbrCarteGenere",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "NbrJour",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "NomPackage",
+                table: "Packages");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateInscription",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateSouscription",
+                table: "Subscriptions",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "SubscriberHistories",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxCarte",
+                table: "Packages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateExpirations",
+                table: "Packages",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "MerchantHistories",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "BeneficiaryHistories",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
